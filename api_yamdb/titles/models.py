cd0d02abeea35bd8c6681dd042e0b76a38d94ec1
Dmitry-russ <dapetuhov@yandex.ru>
2022-07-13 22:23:03 +0500

    Создал модели Titles, Genres, Categories.

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -1,8 +1,36 @@
 from django.db import models
 
-class Titles():
+
+class Categories(models.Model):
+    """Категории (типы) произведений."""
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
 
 
-    
-    pass
-# Create your models here.
+class Genres(models.Model):
+    """Категории жанров."""
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Titles(models.Model):
+    """Произведения, к которым пишут отзывы."""
+    name = models.CharField(max_length=200)
+    year = models.IntegerField()
+    categorie = models.ForeignKey(
+        Categories, on_delete=models.SET_NULL,
+        related_name="titles", blank=True, null=True
+    )
+    genre = models.ManyToManyField(
+        Genres, related_name="titles",
+        blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.name
