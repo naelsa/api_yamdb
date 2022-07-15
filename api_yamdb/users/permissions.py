594ac5e9c0269e922df39318a612c9170b273d0c
Константин Тетерин <zmui168@gmail.com>
2022-07-15 19:00:33 +0300

    миграции 08

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -1,11 +1,16 @@
 from rest_framework import permissions
 
 
+# class IsAdmin(permissions.BasePermission):
+#     """Редактирование объекта доступно админу."""
+#
+#     def has_permission(self, request, view):
+#         return request.user.is_authenticated and request.user.is_admin
+#
 class IsAdmin(permissions.BasePermission):
-    """Редактирование объекта доступно админу."""
-
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.is_admin
+        return (request.user.is_authenticated
+                and request.user.is_admin)
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,5 +1,5 @@
-from rest_framework import viewsets, status
-from rest_framework.decorators import action
+from rest_framework import viewsets, status, filters
+from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
 
 from users.models import User
@@ -16,22 +16,30 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated, IsSuperuser or IsAdmin]
+
+    permission_classes = [IsAuthenticated, IsAdmin | IsSuperuser]
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('username',)
     lookup_field = 'username'
 
     @action(methods=['GET', 'PATCH'], detail=False,
             permission_classes=[IsAuthenticated], url_path='me')
     def user_me(self, request):
-        if request.method == 'GET':
+        if request.method != 'PATCH':
             serializer = self.get_serializer(request.user)
             return Response(serializer.data, status=status.HTTP_200_OK)
-        if request.method == 'PATCH':
-            serializer = self.get_serializer(request.user,
-                                             data=request.data,
-                                             partial=True)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
+        serializer = self.get_serializer(request.user, data=request.data,
+                                         partial=True)
+        serializer.is_valid(raise_exception=True)
+        if serializer.validated_data.get('role'):
+            serializer.validated_data['role'] = request.user.role
+        serializer.save()
+        return Response(serializer.data)
 
-
+#
+# @api_view(['POST'])
+# def signup_user(request):
+#     serializer = RegTokSerializer(data=request.data)
+#     serializer.is_valid(raise_exception=True)
+#     username = serializer.validated_data.get('username')
+#     email = serializer.validated_data.get('email')
