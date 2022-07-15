569182849f18dd13b681ef4276d7d39b0d70a9d0
Константин Тетерин <zmui168@gmail.com>
2022-07-15 15:23:11 +0300

    Исправлена модель,сделан юзерВьюСет, константы перенесены в сеттингс, сделана настройка отправления емейла

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,7 +1,6 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-from api_yamdb import settings
 from .generate_code import generate_confirmation_code
 
 
