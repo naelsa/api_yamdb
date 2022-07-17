a62e7414ba76466ba226a9286ed2791e4af754b7
Dmitry-russ <dapetuhov@yandex.ru>
2022-07-17 12:07:06 +0500

    не коммитится views почему то

--- api_yamdb/titles/views.py
+++ api_yamdb/titles/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
