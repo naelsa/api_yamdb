dc22078e76169921960012f7185d6110aef04f7d
Dmitry-russ <dapetuhov@yandex.ru>
2022-07-20 16:37:06 +0500

    поправил замечания по users

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -33,9 +33,7 @@ class UserViewSet(viewsets.ModelViewSet):
             request.user, data=request.data, partial=True
         )
         serializer.is_valid(raise_exception=True)
-        if serializer.validated_data.get('role'):
-            serializer.validated_data['role'] = request.user.role
-        serializer.save()
+        serializer.save(role=request.user.role)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
